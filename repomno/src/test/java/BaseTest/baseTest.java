b5d9982588443dc4850f79918246e2c21f159c74
sai <sainaidudodda777@gmail.com>
2024-04-19 11:34:23 +0530

    kkk

--- repomno/src/test/java/BaseTest/baseTest.java
+++ repomno/src/test/java/BaseTest/baseTest.java
@@ -3,7 +3,7 @@ package BaseTest;
 import org.testng.annotations.Test;
 
 public class baseTest {
-System.out.println("sairamdodda777@gmail.com);
+System.out.println("sairamdodda777@gmail.com777777777777777);
 
 	@Test
 	public void adminlogin() {
